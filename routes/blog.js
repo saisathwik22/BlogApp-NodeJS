f2fac8d2b522d3193dcce0ba146bb762d60b1408
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-05 20:53:50 +0530

    adding comments and displaying comments.

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -26,11 +27,25 @@ router.get("/add-new", (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const blog = await Blog.findById(req.params.id);
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  console.log("comments", comments);
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage"), (req, res) => {
