7099c88074d6c80f4314314a517400aa1d5330ce
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-05 15:39:35 +0530

    Adding Blog with img and text info.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const Blog = require("./models/blog");
+
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
 const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication");
@@ -21,13 +25,17 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
