3809aa83e4b9274035eaf3d24db51981863f0de7
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-05 10:36:04 +0530

    setting up project, navbar, signup, signin forms, password hashing

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://localhost:27017/blogify")
+  .then((e) => console.log("MongoDb connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use("/user", userRoute);
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
