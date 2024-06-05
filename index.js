85befc76afa65a2a5b5bf57c4e9833db7014b268
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-06-05 13:58:45 +0530

    create account, signin, logout buttons set-up

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const userRoute = require("./routes/user");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -15,9 +19,13 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    user: req.user,
+  });
 });
 
 app.use("/user", userRoute);
